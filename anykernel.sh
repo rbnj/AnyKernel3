9ed76038a71c43333150b20bfb4bcc269d091972
BladeRunner-A2C <prisoner56898@protonmail.com>
2021-10-24 02:09:23 +0600

    Welcome OathKeeper
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,18 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=NotKernel by imjyotiraditya
+kernel.string=OathKeeper Kernel by Bond
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=miatoll
-device.name2=
-device.name3=
-device.name4=
-device.name5=
-supported.versions=10.0-11.0
+device.name2=curtana
+device.name3=excalibur
+device.name4=gram
+device.name5=joyeuse
+supported.versions=11.0-12.0
 supported.patchlevels=
 '; } # end properties
 
